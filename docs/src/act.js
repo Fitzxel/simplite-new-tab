6b2429585ea16d3f9d67f394000bdcd1cc1698ad
Fitzxel <87250719+Fitzxel@users.noreply.github.com>
2022-09-02 18:14:21 -0300

    optimized and fix

--- docs/src/act.js
+++ docs/src/act.js
@@ -6,7 +6,7 @@ document.title = `${docData.title} - Fitzxel`;
 // set page description
 document.head.querySelector('meta[name="description"]').setAttribute('content', docData.desc);
 
-// page theme scripts
+// page theme
 if (!localStorage.getItem('theme')) {
     localStorage.setItem('theme', 'auto');
 }
@@ -46,7 +46,7 @@ slctThemeBtns.forEach(btn => {
     });
 });
 
-// page lang scripts
+// page lang
 if (!localStorage.getItem('lang')) {
     if (window.navigator.language.includes('es')) {
         localStorage.setItem('lang', 'es');
@@ -78,54 +78,7 @@ slctLangBtns.forEach(btn => {
     });
 });
 
-let indexN = 5;
-
-// search scripts
-
-let indexData;
-// if on line
-if (window.navigator.onLine == true) {
-    // get search index
-    fetch('https://fitzxel.github.io/data/fx-index.json').then(res=> {
-        if (res.status >= 200 && res.status <= 299) {
-            res.json().then(res=> {
-                indexData = res;
-                // create and get search.js
-                let searchScript = createElement('script');
-                searchScript.src = 'https://fitzxel.github.io/scripts/search.js';
-                document.body.appendChild(searchScript);
-                // create and get projects-nav.js
-                let projectsNavScript = createElement('script');
-                projectsNavScript.src = 'https://fitzxel.github.io/scripts/projects-nav.js';
-                document.body.appendChild(projectsNavScript);
-            });
-        }
-        else if (res.status >= 400 && res.status <= 499) {
-            searchResults.querySelector('.over-text').textContent = 'An error has occurred';
-        }
-    });
-}
-
-// page nav scripts
-let navOpen = false;
-// const navMenu = document.querySelector('#navigation-menu');
-
-// document.querySelector('#menu-btn').addEventListener('click', ()=> {
-//     if (navOpen) {
-//         navMenu.classList.remove('open');
-//         navOpen = false;
-//     }
-//     else {
-//         navMenu.classList.add('open');
-//         navOpen = true;
-//     }
-// });
-
-// actions on the body
-function closeProjectsNav() {
-    // navMenu.classList.remove('open');
-    navOpen = false;
-}
+// close all switchs function
 function closeSwitchs() {
     if (swtThemeBtn.parentElement.classList[1] == 'act') {
         swtThemeBtn.parentElement.classList.remove('act');
@@ -143,49 +96,7 @@ function closeSwitchs() {
     }
 }
 
-document.body.addEventListener('keydown', (e)=>{
-    if (e.key == 'Escape') {
-        e.preventDefault();
-        closeProjectsNav();
-        closeSwitchs();
-    }
-});
-document.body.addEventListener('click', (e)=>{
-    if (!e.composedPath().find(element => element.nodeName == 'NAV') && navOpen && !e.composedPath().find(element => element.nodeName == 'HEADER')) {
-        closeProjectsNav();
-    }
-    if (!e.composedPath().find(element => element.nodeName == 'BUTTON')) {
-        closeSwitchs();
-    }
-});
-
-// apply an icon to all "a" items that open in a new tab
-const allA = document.querySelectorAll('a[target="_blank"]');
-allA.forEach(a => {
-    let icon = createElement('ion-icon');
-    icon.name = 'open';
-    icon.classList.add('a-icon');
-    a.appendChild(icon);
-});
-
-let asideOpen = false;
-const aside = document.querySelector('.aside');
-
-document.querySelector('#aside-btn').addEventListener('click', ()=> {
-    if (asideOpen) {
-        aside.classList.remove('open');
-        asideOpen = false;
-    }
-    else {
-        aside.classList.add('open');
-        asideOpen = true;
-    }
-});
-
-document.querySelector('#docsite-banner').addEventListener('load', ()=> {
-    document.querySelector('#docsite-banner').style.opacity = '1';
-});
-
+// set article metadata from docData.js
 document.querySelector('.metadata').querySelector('.icon').alt = docData.tags;
 document.querySelector('.metadata').querySelector('h1').textContent = docData.title;
 document.querySelector('.metadata').querySelector('time').dateTime = `${docData.lastDate.getFullYear()}-${docData.lastDate.getMonth()+1}-${docData.lastDate.getDate()}`
@@ -195,7 +106,6 @@ document.querySelector('#desc').textContent = `'${docData.desc}'`;
 document.querySelector('#repo').href = docData.repo;
 document.querySelector('#repo').innerHTML = docData.repo;
 document.querySelector('#type').textContent = `'${docData.type}'`;
-
 // set languages
 let documentFragmentLangs = document.createDocumentFragment();
 for (let i = 0; i < docData.programing_langs.length; i++) {
@@ -213,7 +123,6 @@ for (let i = 0; i < docData.programing_langs.length; i++) {
     documentFragmentLangs.appendChild(li);
 }
 document.querySelector('#append-langs ul').appendChild(documentFragmentLangs);
-
 // set getIts
 let documentFragmentGetIt = document.createDocumentFragment();
 for (let i = 0; i < docData.getIt.length; i++) {
@@ -231,27 +140,35 @@ for (let i = 0; i < docData.getIt.length; i++) {
     documentFragmentGetIt.appendChild(li);
 }
 document.querySelector('#append-getIts ul').appendChild(documentFragmentGetIt);
-
 // set licence text
 document.querySelector('#licence').textContent = `'${docData.licence}'`;
 
-// article h2
+// declare setTabIndex function
+function setTabIndex() {
+    // select all element with tabindex class
+    const allE = document.querySelectorAll('.tabindex');
+    // set tabIndex attribute for all elements with have tabindex class
+    for (let i = 0; i < allE.length; i++) {
+        allE[i].tabIndex = i+1;
+    }
+}
+
+// create "li" elements for each "h2" element
 const articleH2 = document.querySelectorAll('.article-body .content h2');
 
-// create asides li
 articleH2.forEach(h2 => {
     let li = createElement('li');
     let a = createElement('a');
     a.href = `#${h2.id}`;
     a.textContent = h2.textContent;
-    a.classList.add('tabindex');
+    a.classList.add('type-1', 'tabindex');
     li.appendChild(a);
     // append li in aside nav
     document.querySelector('.aside .nav').appendChild(li);
 });
 
-let lastIntersectingScroll = 0;
 // callback for observer
+let lastIntersectingScroll = 0;
 // when im comment about "entry" im refer to h2 intersecting and to "a" element on the "aside"
 function callback(entries, observer) {
     entries.forEach(entry => {
@@ -259,10 +176,10 @@ function callback(entries, observer) {
         if (!entry.isIntersecting && entry.intersectionRatio < .5 && lastIntersectingScroll > window.scrollY) {
             // console.log(entry);
             // console.log(`salir de ${entry.target.id}`);
-            document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.classList.remove('act');
+            document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).classList.remove('act');
             // and if this entry have a previous element, add "act" class to this previous element
             if (document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling) {
-                document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling.querySelector('a').parentElement.classList.add('act');
+                document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling.querySelector('a').classList.add('act');
             }
         }
         // if entry is intersecting add "act" class on this entry
@@ -271,10 +188,10 @@ function callback(entries, observer) {
             // console.log(`entrar a ${entry.target.id}`);
             // set current value of scrollY on lastIntersectionScroll
             lastIntersectingScroll = window.scrollY;
-            document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.classList.add('act');
+            document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).classList.add('act');
             // and if this entry have a previous element, remove "act" class to this previous element
             if (document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling) {
-                document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling.querySelector('a').parentElement.classList.remove('act');
+                document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.previousElementSibling.querySelector('a').classList.remove('act');
             }
             // if (document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.nextElementSibling) {
             //     document.querySelector(`.aside ul.nav li a[href="#${entry.target.id}"]`).parentElement.nextElementSibling.querySelector('a').parentElement.classList.remove('act');
@@ -292,14 +209,93 @@ articleH2.forEach(h2 => {
     observer.observe(h2);
 });
 
+// dropdown (projects-nav and the aside)
+let dropdownOpen = false;
 
-function setTabIndex() {
-    // select all element with tabindex class
-    const allE = document.querySelectorAll('.tabindex');
-    // set tabIndex attribute for all elements with have tabindex class
-    for (let i = 0; i < allE.length; i++) {
-        allE[i].tabIndex = i+1;
+document.querySelector('#menu-btn').addEventListener('click', ()=> {
+    if (!dropdownOpen) {
+        if (window.innerWidth <= 600) {
+            document.querySelector('.projects-nav').style.transform = `translateY(${document.querySelector('.aside').offsetHeight}px)`;
+        }
+        else {
+            document.querySelector('.aside').style.height = `${document.querySelector('.projects-nav').offsetHeight}px`;
+        }
+        document.querySelector('main').classList.add('dropdown-open');
+        dropdownOpen = true;
     }
-}
+    else {
+        document.querySelector('.projects-nav').style.transform = '';
+        document.querySelector('main').classList.remove('dropdown-open');
+        dropdownOpen = false;
+    }
+});
 
-setTabIndex();
+// declare indexData variable
+let indexData;
+
+// events and actions on the document and the body
+document.addEventListener('DOMContentLoaded', ()=> {
+    setTabIndex();
+
+    //if is on line
+    if (window.navigator.onLine == true) {
+        // apply an icon to all "a" items that open in a new tab
+        document.querySelectorAll('a[target="_blank"]').forEach(a => {
+            let icon = createElement('span');
+            icon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon a-icon" viewBox="0 0 512 512">
+                <title>Open</title>
+                <path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
+            </svg>`
+            a.appendChild(icon);
+        });
+        // get index.json
+        fetch('https://fitzxel.github.io/data/fx-index.json').then(res=> {
+            if (res.status >= 200 && res.status <= 299) {
+                res.json().then(res=> {
+                    indexData = res;
+                    // create and get search.js
+                    let searchScript = createElement('script');
+                    searchScript.src = 'https://fitzxel.github.io/scripts/search.js';
+                    document.body.appendChild(searchScript);
+                    // create and get projects-nav.js
+                    let projectsNavScript = createElement('script');
+                    // projectsNavScript.src = 'https://fitzxel.github.io/scripts/projects-nav.js';
+                    projectsNavScript.src = 'http://127.0.0.1:5500/scripts/projects-nav.js';
+                    document.body.appendChild(projectsNavScript);
+                });
+            }
+            else if (res.status >= 400 && res.status <= 499) {
+                searchResults.querySelector('.over-text').textContent = 'An error has occurred';
+            }
+        });
+    }
+    else console.log('not online');
+});
+document.body.addEventListener('keydown', (e)=>{
+    if (e.key == 'Escape') {
+        e.preventDefault();
+        closeProjectsNav();
+        closeSwitchs();
+    }
+});
+document.body.addEventListener('click', (e)=>{
+    if (!e.composedPath().find(element => element.nodeName == 'NAV') && !e.composedPath().find(element => element.nodeName == 'ASIDE') && dropdownOpen && !e.composedPath().find(element => element.nodeName == 'HEADER')) {
+        document.querySelector('main').classList.remove('dropdown-open');
+    }
+    if (!e.composedPath().find(element => element.nodeName == 'BUTTON')) {
+        closeSwitchs();
+    }
+});
+
+// window events
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth <= 600 && dropdownOpen) {
+        document.querySelector('.projects-nav').style.transform = `translateY(${document.querySelector('.aside').offsetHeight}px)`;
+        document.querySelector('.aside').style.height = '';
+    }
+    else if (window.innerWidth > 600) {
+        document.querySelector('.projects-nav').style.transform = '';
+        document.querySelector('.aside').style.height = `${document.querySelector('.projects-nav').offsetHeight}px`;
+    }
+});
